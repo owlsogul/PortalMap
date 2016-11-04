7cf8244aed9fa7f9ca27d7ff70e062439a4adeb9
owlsogul <owlsogul@naver.com>
2016-11-05 02:17:58 +0900

    PortalManager

---
+++ app/src/main/java/com/example/user/portalmap/BuildingData.java
@@ -0,0 +1,20 @@
+package com.example.user.portalmap;
+
+/**
+ * Created by user on 2016-11-05.
+ */
+
+public class BuildingData {
+
+    int code;
+    int x1, y1, x2, y2;
+    public BuildingData(int code, int x1, int y1, int x2, int y2){
+        this.code = code;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+
+}

eba688c1992eea0dcf72637acf45169c0c61a488
owlsogul <owlsogul@naver.com>
2016-11-04 19:49:51 +0900

    test1

--- app/src/main/java/com/example/user/portalmap/MainActivity.java
+++ app/src/main/java/com/example/user/portalmap/MainActivity.java
@@ -5,9 +5,6 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

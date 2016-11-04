f8cd2a7f9be2763ffcf2f50c838965647e8e4fa2
owlsogul <owlsogul@naver.com>
2016-11-04 20:00:51 +0900

    test3

--- app/src/main/java/com/example/user/portalmap/MainActivity.java
+++ app/src/main/java/com/example/user/portalmap/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.user.portalmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    /////////
+    Button bu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/example/user/portalmap/PortalData.java
@@ -0,0 +1,15 @@
+package com.example.user.portalmap;
+
+/**
+ * Created by user on 2016-11-04.
+ */
+
+public class PortalData {
+
+    //연결된 건물의 관을 저장하는 배열
+    protected int edgeBuilding[] = {0 , 0};
+
+    //
+    protected int portalType;
+
+}

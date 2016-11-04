7847ed170e103da6544e27f97bec812fbaafc41e
owlsogul <owlsogul@naver.com>
2016-11-04 19:48:32 +0900

    test1

--- app/src/main/java/com/example/user/portalmap/MainActivity.java
+++ app/src/main/java/com/example/user/portalmap/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.user.portalmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Log.i("s","s");
+        //Log.i("s","s");asd
+
+
     }
 }

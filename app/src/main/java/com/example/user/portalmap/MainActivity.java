307f8a3580ce30a442be2cc42366f1f3efb48456
simba328 <simba328@naver.com>
2016-11-05 00:13:36 +0900

    change2

--- app/src/main/java/com/example/user/portalmap/MainActivity.java
+++ app/src/main/java/com/example/user/portalmap/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.user.portalmap;
 
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     public PortalManager pManager;
 
@@ -15,10 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("TEST", "create");
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        Log.i("TEST", "help");
         help = (Button) findViewById(R.id.help);
+        Log.i("TEST", "all");
         allPortal = (Button) findViewById(R.id.all);
+        Log.i("TEST", "map");
         iView = (ImageView) findViewById(R.id.map);
 
         new AsyncTask<Object, Object, Void>(){

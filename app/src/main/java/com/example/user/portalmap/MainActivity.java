7bc35d24da6570535514be96a484ab22c1788142
simba328 <simba328@naver.com>
2016-11-04 19:59:43 +0900

    change2

--- app/src/main/java/com/example/user/portalmap/MainActivity.java
+++ app/src/main/java/com/example/user/portalmap/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Log.i("s","s");asd
-        int change;
+        int change2;
 
     }
 }

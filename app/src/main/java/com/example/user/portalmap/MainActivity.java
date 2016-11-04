78edc61dba197aa4234fb17a502a4464f8b7586c
owlsogul <owlsogul@naver.com>
2016-11-04 23:49:53 +0900

    PortalManager

--- app/src/main/java/com/example/user/portalmap/MainActivity.java
+++ app/src/main/java/com/example/user/portalmap/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.user.portalmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public PortalManager pManager;
+
+    public Button help, allPortal;
+    public ImageView iView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Log.i("s","s");asd
+        pManager = new PortalManager();
+
+        help = (Button) findViewById(R.id.help);
+        allPortal = (Button) findViewById(R.id.all);
+        iView = (ImageView) findViewById(R.id.map);
+
+
     }
 }
